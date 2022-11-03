06b9315aa0ba55f63479717a857bb22ad06389d2
Luis Fernando León Machaca <113323701+fernando301097@users.noreply.github.com>
2022-11-03 17:54:23 -0500

    añadí opcion 1.
    añadí libreri pandas y definí función de lectura csv

--- main proyecto1.py
+++ main proyecto1.py
@@ -1,1 +1,15 @@
-print('hola silabuz')
+import pandas as pd
+#para inporta archivo csv 
+basedatoss = pd .read_csv(r'C:\Users\Fernando Leon\Desktop\ramaluis_2\csv\libros.csv')
+
+
+#opcion 1 mostrar libros guardados
+def mostrar_libros_guardados():
+            print("********** ESTOS SON NUESTROS LIBROS GUARDADOS **********")
+            print(basedatoss)
+
+            s = int(input("presione 0 para volver al menú principal: "))
+            print("")
+
+            while s != 0:
+             menu()
